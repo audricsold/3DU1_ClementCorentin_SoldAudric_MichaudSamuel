b28da560378a50eeccdc0bbab31cb8231b0a1246
Cocodrilo <corentin.clement@epita.fr>
2022-06-16 00:07:08 +0200

    parcours

---
+++ Parcours/3DU1/Assets/menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
+
